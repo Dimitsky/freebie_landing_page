911cb00bd79b61fbbb3e0e02f1ef59e63724832b
Dimitsky <dimitsky@icloud.com>
2022-07-18 18:14:22 +0400

    Добавил стили и адаптив для блока testimonial

--- dest/assets/js/script.js
+++ dest/assets/js/script.js
@@ -1,7 +1,7 @@
 // js
 document.documentElement.classList.add( 'js' );
 
-const swiper = new Swiper('.feature__swiper', {
+const swiperFeature = new Swiper('.feature__swiper', {
     slidesPerView: "auto",
     spaceBetween: 15,
     breakpoints: {
@@ -9,4 +9,15 @@ const swiper = new Swiper('.feature__swiper', {
             spaceBetween: 40,
         }
     }
+});
+
+const swiperTestimonial = new Swiper('.testimonial__swiper', {
+    slidesPerView: 1,
+    spaceBetween: 15,
+
+    navigation: {
+        nextEl: '.nav-swiper-testimonial__btn--next',
+        prevEl: '.nav-swiper-testimonial__btn--prev',
+        disabledClass: 'nav-swiper-testimonial__btn--disabled', 
+      },
 });
