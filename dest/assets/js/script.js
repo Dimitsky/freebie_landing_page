f0d8c3ac18c59e986954d8e58c8f5aad0028d95c
Dimitsky <dimitsky@icloud.com>
2022-07-20 16:38:53 +0400

    bug fix

--- dest/assets/js/script.js
+++ dest/assets/js/script.js
@@ -122,3 +122,115 @@ if ( !mediaQuery.matches ) {
 trigger.classList.add( 'js' );
 menu.classList.add( 'js' );
 mediaQuery.addEventListener( 'change', handleChange );
+
+// EXPLORE ALL 
+
+const count = 4;
+const serviceArr = Array.from( document.querySelectorAll( '.list-service__item' ) );
+const serviceBtn = document.querySelector( '#explore-all' );
+
+serviceBtn.style.display = 'block';
+
+serviceArr.slice( count ).forEach( itemEl => {
+    itemEl.style.display = 'none';
+} );
+
+function serviceBtnHandle( event ) {
+    serviceArr.slice( count ).forEach( itemEl => {
+        itemEl.style.display = 'flex';
+    } );
+
+    serviceBtn.removeEventListener( 'click', serviceBtnHandle );
+    serviceBtn.removeEventListener( 'touchstart', serviceBtnHandle );
+    serviceBtn.style.display = 'none';
+
+    if ( event.type === 'touchstart' ) {
+        event.preventDefault();
+    }
+}
+
+serviceBtn.addEventListener( 'click', serviceBtnHandle );
+serviceBtn.addEventListener( 'touchstart', serviceBtnHandle );
+
+// FAQ ACC
+
+let accEl = document.querySelector( '.acc' );
+let options = {
+    showMultiple: false, 
+    openIndex: 0, 
+};
+
+function init( accEl ) {
+    let itemEls = accEl.querySelectorAll( '.acc__item' );
+    let itemArr = Array.from( itemEls );
+
+    itemArr.forEach( (itemEl, index) => {
+        let contentEl = itemEl.querySelector( '.acc__content' );
+
+        contentEl.classList.add( 'acc__content--js' );
+
+        if ( index === options.openIndex ) {
+          openAcc( itemEl );
+        }
+    } );
+
+    let controlEls = accEl.querySelectorAll( '.acc__control' );
+    let controlArr = Array.from( controlEls );
+
+    controlArr.forEach( controlEl => {
+        controlEl.addEventListener( 'click', handleControl );
+        controlEl.addEventListener( 'touchstart', handleControl );
+    } );
+}
+
+function handleControl( event ) {
+    let isExpanded = this.getAttribute( 'aria-expanded' ) === 'true' ? true : false;
+    let itemEl = this.closest( '.acc__item' );
+
+    if ( isExpanded ) {
+        closeAcc( itemEl );
+    } else {
+        openAcc( itemEl );
+    }
+
+    if ( event.type === 'touchstart' ) {
+        event.preventDefault();
+    }
+}
+
+function openAcc( itemEl ) {
+    let contentEl = itemEl.querySelector( '.acc__content' );
+    let controlEl = itemEl.querySelector( '.acc__control' );
+    let iconEl = itemEl.querySelector( '.acc__icon' );
+
+    if ( options.showMultiple === false ) {
+        closeAllAcc( accEl );
+    }
+
+    contentEl.classList.add( 'acc__content--open' );
+    contentEl.style.maxHeight = contentEl.scrollHeight + 'px';
+    controlEl.setAttribute( 'aria-expanded', 'true');
+    iconEl.classList.add( 'acc__icon--open' );
+}
+
+function closeAcc( itemEl ) {
+    let contentEl = itemEl.querySelector( '.acc__content' );
+    let controlEl = itemEl.querySelector( '.acc__control' );
+    let iconEl = itemEl.querySelector( '.acc__icon' );
+
+    contentEl.classList.remove( 'acc__content--open' );
+    contentEl.style.maxHeight = null;
+    controlEl.setAttribute( 'aria-expanded', 'false');
+    iconEl.classList.remove( 'acc__icon--open' );
+}
+
+function closeAllAcc( accEl ) {
+    let itemEls = accEl.querySelectorAll( '.acc__item' );
+    let itemArr = Array.from( itemEls );
+
+    itemArr.forEach( itemEl => {
+        closeAcc( itemEl );
+    } );
+}
+
+init( accEl );
